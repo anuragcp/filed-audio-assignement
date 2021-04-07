7972dd71d67a3a1883a5001461300498c0bb6f9f
anuragcp <anudeveloper777@gmail.com>
2021-04-07 18:08:54 +0530

    swagger ui - GET, DETETE [completed] PUT, POST [dependency resolving]

--- manage.py
+++ manage.py
@@ -1,10 +1,26 @@
 # coding: utf-8
 from flask_script import Manager
 from application import create_app
-import application.models as model
+from flask_swagger_ui import get_swaggerui_blueprint
 # Used by app debug & livereload
 app = create_app()
 print("app created")
+
+### swagger specific ###
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "filed-audio-assignment"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+### end swagger specific ###
+
+
+
 manager = Manager(app)
 @manager.command
 def run():
